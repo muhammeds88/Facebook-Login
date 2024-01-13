6f3109afd8f89e9e27057630f0fd2f4d972a9fd5
MUHAMMED.S <147074256+muhammeds88@users.noreply.github.com>
2024-01-13 04:11:03 -0500

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,5 @@
+// Function to be called when the "Log in" button is clicked
+function myfun_1() {
+    // Change the inner HTML of the "Log in" button to indicate logging in
+    document.getElementById("log_in").innerHTML = "Logging In...";
+}
